c989e5147a2c90285bdf3f8f9ba8627e27a92765
noel <noelsoong@live.com>
2019-08-01 14:10:38 +1200

    Componentize App

--- src/App.js
+++ src/App.js
@@ -1,36 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Cv from './Cv'
+import EditCV from './EditCV'
+
+const lorem = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+`
+const data = {
+  name: "Noel Soong",
+  profession: "Web Developer",
+  contacts: {
+    email: "noelsoongdev@gmail.com",
+    mobile: "+64 0273462997"
+  },
+  profile: lorem,
+  exprience: [
+    {
+      title: "Experience 1",
+      from: 1243321,
+      to: 4321412,
+      text: lorem
+    },
+    {
+      title: "Experience 2",
+      from: 1243321,
+      to: 4321412,
+      text: lorem
+    },
+    {
+      title: "Experience 3",
+      from: 1243321,
+      to: 4321412,
+      text: lorem
+    },
+    {
+      title: "Experience 4",
+      from: 1243321,
+      to: 4321412,
+      text: lorem
+    },
+  ],
+  education: [
+    {
+      title: "Education 1",
+      from: 1243321,
+      to: 4321412,
+      text: lorem
+    },
+    {
+      title: "Education 2",
+      from: 1243321,
+      to: 4321412,
+      text: lorem
+    },
+  ],
+  references: []
+}
+
+export const CVContext = React.createContext(data)
 
 function App() {
+
+  const [showPreview, setShowPreview] = useState(false)
+
   return (
     <div className="App">
-      <div className="cv">
-        <div className="col1">
-          <div className="title">
-            <h1>Noel Soong</h1>
-            <span>Web Developer</span>
-          </div>
-          <div className="paragraph">
-            <h3>Profile</h3>
-            <p></p>
-          </div>
-          <div className="paragraph">
-            <h3>Experience</h3>
-            <p></p>
-          </div>
-          <div className="paragraph">
-            <h3>Education</h3>
-            <p></p>
-          </div>
-          <div className="paragraph">
-            <h3>References</h3>
-            <p>Available upon Request</p>
-          </div>
-        </div>
-        <div className="col2">
-
-        </div>
-      </div>
+     {
+       showPreview ? <Cv/> : <EditCV/>
+     }
     </div>
   );
 }
